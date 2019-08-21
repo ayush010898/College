a1e576a9a30c3ce85d5b6fb2226e86e471ec1470
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2019-08-21 12:43:46 +0530

    Add files via upload

---
+++ IOT_LAB/temperatureSensor.ino
@@ -0,0 +1,15 @@
+int outputpin = 3;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int rawvoltage = analogRead(outputpin);
+  float millivolts = (rawvoltage/1024.0)*5000;
+  float celsius = millivolts/10;
+  Serial.print(celsius);
+  Serial.print("degree celsius");
+  Serial.print((celsius*9)/5 + 32);
+  Serial.print("degree fahrenheit");
+  delay(1000);
+}
