528b464d4537acc319f3ce1e12163c695450389e
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2020-05-04 21:43:17 +0530

    Update MainActivity.java

--- MAD LAB/1BM17CS018/lab 9/MainActivity.java
+++ MAD LAB/1BM17CS018/lab 9/MainActivity.java
@@ -1,41 +1,51 @@
-Packagecom.example.exno10;
-Importandroid.app.Notification;
-Importandroid.app.NotificationManager;
-Importandroid.app.PendingIntent;
-Importandroid.content.Intent;
-Importandroid.os.Bundle;
-Importandroid.support.v7.app.AppCompatActivity;
-Importandroid.view.View;
-Importandroid.widget.Button;
-Importandroid.widget.EditText;
-PublicclassMainActivity extendsAppCompatActivity
-{
-Button notify;
-EditText e;
-@Override
-ProtectedvoidonCreate(Bundle savedInstanceState)
-{
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main);
-notify= (Button) findViewById(R.id.button);
-e= (EditText) findViewById(R.id.editText);
-notify.setOnClickListener(newView.OnClickListener()
-{
-@Override
-PublicvoidonClick(View v)
-{
-Intent intent = newIntent(MainActivity.this, SecondActivity.class);
-PendingIntent pending = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+package com.example.example;
 
-Notification noti = newNotification.Builder(MainActivity.this).setContentTitle("New
-Message").setContentText(e.getText().toString()).setSmallIcon(R.mipmap.ic_launcher).setCont
-entIntent(pending).build();
-NotificationManager manager = (NotificationManager)
-getSystemService(NOTIFICATION_SERVICE);
-noti.flags |= Notification.FLAG_AUTO_CANCEL;
-manager.notify(0, noti);
-}
-});
-}
-}
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.message);
+        Button buttonNotify = findViewById(R.id.buttonNotify);
+        buttonNotify.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String notificationChannelId = "channel_id";
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel notificationChannel = new NotificationChannel(notificationChannelId, "channel_name", NotificationManager.IMPORTANCE_DEFAULT);
+
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(notificationChannel);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, notificationChannelId);
+            notificationBuilder
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("New Message")
+                    .setContentText(message.getText().toString());
+
+            notificationManager.notify(1, notificationBuilder.build());
+        } else {
+            Toast.makeText(this, "Unable to create notification channel.", Toast.LENGTH_LONG).show();
+        }
+    }
 }
