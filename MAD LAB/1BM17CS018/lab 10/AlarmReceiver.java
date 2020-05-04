41b95280003042be1207974396a246e50c69d0c1
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2020-05-04 15:26:40 +0530

    Update AlarmReceiver.java

--- MAD LAB/1BM17CS018/lab 10/AlarmReceiver.java
+++ MAD LAB/1BM17CS018/lab 10/AlarmReceiver.java
@@ -1,4 +1,5 @@
 package com.example.exno11;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -6,18 +7,19 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.widget.Toast;
+
 public class AlarmReceiver extends BroadcastReceiver
 {
-@Override
-public void onReceive(Context context, Intent intent)
-{
-Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-if(alarmUri == null)
-
-{
-alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-}
-Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-ringtone.play();
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
 }
