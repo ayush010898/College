adfa1f720bb062564d4cddd3dce494727c5777a9
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2020-10-06 13:04:06 +0530

    Add files via upload

---
+++ BDA_LAB/lab 3/lab 3(a).js
@@ -0,0 +1,20 @@
+use Database
+db.createCollection("Customer")
+db.customer.insert({cust_id:1,acc_bal:1500,acc_type:"z"})
+db.customer.insert({cust_id:2,acc_bal:1800,acc_type:"a"})
+db.customer.insert({cust_id:1,acc_bal:2500,acc_type:"z"})
+db.customer.insert({cust_id:2,acc_bal:1500,acc_type:"a"})
+db.customer.insert({cust_id:1,acc_bal:5500,acc_type:"z"})
+db.customer.find().pretty()
+db.customer.find({acc_bal:{$gt:1200}, acc_type:"z"})
+db.createCollection("extra")
+db.extra.drop()
+db.customer.aggregate([
+    {
+        $group: {
+            _id: "$cust_id",
+            min_bal: {$min: "$acc_bal"},
+            max_bal: {$max: "$acc_bal"}
+        }
+    }
+])
---
+++ BDA_LAB/lab 3/lab 3(b).js
@@ -0,0 +1,18 @@
+show dbs
+use Student
+db.createCollection("details")
+db.details.insert({_id: 1,name: "Ayush", RollNo: 10, Age: 22, ContactNo: 8547125698, EmailId: "a@g.com"})
+db.details.insert({_id: 2,name: "ABC", RollNo: 11, Age: 25, ContactNo: 7981295698, EmailId: "b@h.com"})
+db.details.insert({_id: 3,name: "XYZ", RollNo: 15, Age: 28, ContactNo: 9955784126, EmailId: "c@d.com"})
+db.details.find().pretty()
+db.details.update({RollNo: 10},{$set:{EmailId : "ayush@gmail.com"}})
+db.details.find().pretty()
+db.details.replaceOne({RollNo: 11}, {_id: 2,name: "FEM",Age: 25, ContactNo: 7981295698,EmailId:"b@h.com"})
+db.details.find().pretty()
+db.createCollection("HostelDeatails")
+db.HostelDetails.drop()
+show collections
+
+
+
+
