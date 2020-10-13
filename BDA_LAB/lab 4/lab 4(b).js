49d11cf16830a189bd78a26db83fb531fce81c37
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2020-10-13 12:55:06 +0530

    Add files via upload

---
+++ BDA_LAB/lab 4/lab 4(b).js
@@ -0,0 +1,9 @@
+show dbs
+use test
+db.data.find()
+db.data.aggregate([
+    { $group : { _id: null, sum: {$sum:"$age"} } }
+])
+db.data.aggregate([
+    { $group : { _id: "Avg of income", avg: {$avg:"$income"} } }
+])
