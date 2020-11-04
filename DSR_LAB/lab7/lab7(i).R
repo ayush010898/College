23da3b84c0ec6ea77160fe061ba0f4d6f6fabee5
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2020-11-04 15:09:21 +0530

    Update lab7(i).R

--- DSR_LAB/lab7/lab7(i).R
+++ DSR_LAB/lab7/lab7(i).R
@@ -1,4 +1,4 @@
 library(gcookbook)
-str(cabbage_exp)
-c<-table(cabbage_exp$Cultivar)
-ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) +geom_bar(stat="identity", position="dodge") +geom_text(aes(label=Weight), vjust=1.5, colour="white", position=position_dodge(.9), size=3)
+df = cabbage_exp
+head(df)
+barplot(Weight ~ Cultivar+Date, data=df, xlab = "Date", ylab = "weight", beside=TRUE, legend = TRUE,col =c("black","grey"))
